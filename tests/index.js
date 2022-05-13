28ee47d5b4f89dcf9aff388d63b54a0e1728b2ca
Teselka <savi_2005@mail.ru>
2022-05-13 23:29:42 +0300

    import fix

--- tests/index.js
+++ tests/index.js
@@ -2,5 +2,5 @@ const { requests } = require('../lib/index.js');
 
 (async () => {
     const data = await requests.get('https:///example.com/');
-    console.log(data.status);
+    console.log(data.text);
 })();
